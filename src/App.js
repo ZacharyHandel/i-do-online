09581d14a3466953f589762b958aecb1e6dbd9fb
Zachary Handel <zachary.handelzh@gmail.com>
2023-05-19 01:44:41 -0500

    Test Changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="./logo.png" alt="logo not found"/>
-        <h1>To my Beautiful Fiance:</h1>
+        <div className="App-logo">
+          <img src={ require("./images/logo.png")} alt="logo not found"/>
+        </div>
+        <h1 className="i-do-fancy">I Do! Online</h1>
+        <h2>To my Beautiful Fiance:</h2>
         <p>This is the site; I hope you like it. I love you, Kenslie!</p>
       </header>
     </div>
